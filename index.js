cd885d62c30ce03a0729fcfcb87ac4e0224a8e7d
tanvir <shkhtanvirahmed@gmail.com>
2024-07-10 20:00:08 +0600

    Add: projectile

--- index.js
+++ index.js
@@ -28,7 +28,68 @@ class Player{
   }
 }
 
+// Creating a class for Projectile
+class Projectile{
+  constructor(x, y, radius, color, velocity){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
 
+  draw_projectile(){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update(){
+    this.draw_projectile();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
+}
 
 const mainPlayer = new Player(100, 100, 30, "blue");
 mainPlayer.draw();
+
+//const projectile = new Projectile(canvas.width/2, canvas.height/2, 5, "red", {x: 1, y: 1});
+//const projectile2 = new Projectile(canvas.width/2, canvas.height/2, 5, "purple", {x: -1, y: 1});
+//const projectile3 = new Projectile(canvas.width/2, canvas.height/2, 5, "aqua", {x: -1, y: -1});
+//const projectile4 = new Projectile(canvas.width/2, canvas.height/2, 5, "lime", {x: +1, y: -0.5});
+
+// projectiles array
+
+const projectiles = [];
+
+//Animation
+function animate(){
+  window.requestAnimationFrame(animate);
+  //projectile.draw_projectile();
+  //projectile.update();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  mainPlayer.draw();
+  projectiles.forEach((p) => {
+    p.update();
+  })
+}
+
+// Event Listener
+window.addEventListener('click', (event) => {
+  //console.log("event Clicked!!")
+  //console.log(event);
+  
+  // Calculating the angle between main player and mouse Clicked
+  //const angle = Math.atan2(event.clientY, event.clientX);
+  const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2);
+
+  console.log("Angle: ", angle);
+
+  projectiles.push(new Projectile(canvas.width/2, canvas.height/2, 5, "red", {x: Math.cos(angle), y: Math.sin(angle)}))
+
+  animate();
+})
+
