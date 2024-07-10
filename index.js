2363ccfe4d4cc5715c4a7d8a45c1aa9f1a956f99
tanvir <shkhtanvirahmed@gmail.com>
2024-07-11 01:42:26 +0600

    Fix: shooted balls were getting extra velocity at each click

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const canvas = document.querySelector('canvas');
 // selecting the canvas context
 const ctx = canvas.getContext('2d');
 
-canvas.width  = innerWidth
-canvas.height = innerHeight
+canvas.width  = window.innerWidth
+canvas.height = window.innerHeight
 
 const x = canvas.width/2; 
 const y = canvas.height/2; 
@@ -22,8 +22,6 @@ class Player{
     ctx.beginPath();
     ctx.arc(x, y, this.radius, 0 * (Math.PI/180), 360 * (Math.PI/180), false);
     ctx.fillStyle = this.color;
-//    ctx.strokeStyle = this.color;
-//     ctx.stroke();
     ctx.fill();  
   }
 }
@@ -47,29 +45,20 @@ class Projectile{
 
   update(){
     this.draw_projectile();
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
+
+    this.x =+ this.velocity.x;
+    this.y =+ this.velocity.y;
   }
 }
 
 const mainPlayer = new Player(100, 100, 30, "blue");
-mainPlayer.draw();
-
-//const projectile = new Projectile(canvas.width/2, canvas.height/2, 5, "red", {x: 1, y: 1});
-//const projectile2 = new Projectile(canvas.width/2, canvas.height/2, 5, "purple", {x: -1, y: 1});
-//const projectile3 = new Projectile(canvas.width/2, canvas.height/2, 5, "aqua", {x: -1, y: -1});
-//const projectile4 = new Projectile(canvas.width/2, canvas.height/2, 5, "lime", {x: +1, y: -0.5});
 
 // projectiles array
-
 const projectiles = [];
 
 //Animation
 function animate(){
   window.requestAnimationFrame(animate);
-  //projectile.draw_projectile();
-  //projectile.update();
-
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   mainPlayer.draw();
   projectiles.forEach((p) => {
@@ -79,17 +68,11 @@ function animate(){
 
 // Event Listener
 window.addEventListener('click', (event) => {
-  //console.log("event Clicked!!")
-  //console.log(event);
-  
-  // Calculating the angle between main player and mouse Clicked
-  //const angle = Math.atan2(event.clientY, event.clientX);
   const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2);
 
   console.log("Angle: ", angle);
 
   projectiles.push(new Projectile(canvas.width/2, canvas.height/2, 5, "red", {x: Math.cos(angle), y: Math.sin(angle)}))
-
-  animate();
 })
 
+animate();
